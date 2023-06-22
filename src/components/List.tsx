c5616fb18ab202d7ec5656089d9878cafe75dcd8
koionaroll <tranvankhoi2002@gmail.com>
2023-06-21 23:25:22 -0700

    fixed up list styling

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -4,13 +4,15 @@ import { v4 as uuidv4 } from "uuid";
 function List({ list }) {
   return (
     <>
-      {list.map((el) => {
-        return (
-          <>
-            <div key={uuidv4()}>{el}</div>
-          </>
-        );
-      })}
+      <div style={{ display: "flex", gap: "0.5rem", flexDirection: "row-reverse",  justifyContent: "flex-end"}}>
+        {list.map((el) => {
+          return (
+            <>
+              <div key={uuidv4()}>{el}</div>
+            </>
+          );
+        })}
+      </div>
     </>
   );
 }
